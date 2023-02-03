b43a84882fba0653176577ef8b24b91aca5c8266
Tyler <97559004+tylerwyer@users.noreply.github.com>
2023-02-02 21:12:13 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+// any path here is relative to the path that this router is associated with in app.js
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+// -------- Option 1 ---------
+// extend index router
+
+// router.get('/about', (req, res, next) => {
+//   //res.send('PLACEHOLDER FOR ABOUT PAGE');
+//   res.render('about', { title: "About Us" })
+// });
+
+
+
+module.exports = router;
